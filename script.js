97bf2003cd6f6cbaa72e2e792e2866b3f1978fee
bans1mp <iamsarthakgandotra@gmail.com>
2022-01-12 00:11:44 +0530

    Alarm changes

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 setInterval(setClock, 1000)
+const button = document.getElementById('btn')
+const list = document.getElementById('list');
+
 
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
@@ -14,8 +17,33 @@ function setClock() {
     setRotation(hourHand, hoursRatio)
 }
 
+
+
+
+function addAlarm(time) {
+    const li = document.createElement('li');
+    const hrs = Math.floor(time / 100);
+    const minutes = time % 100;
+    if (minutes != 0 && hrs != 0) {
+        li.innerHTML = `${hrs}:${minutes}`
+    } else if (minutes == 0 || hrs == 0) {
+        if (hrs == 0) {
+            li.innerHTML = `00:${minutes}`
+
+        }
+        else {
+            li.innerHTML = `${hrs}:00`
+        }
+
+    }
+
+    list.appendChild(li);
+}
+
 function setRotation(element, rotationRatio) {
     element.style.setProperty('--angle', rotationRatio * 360)
 }
 
 setClock()
+
+addAlarm(0000);
