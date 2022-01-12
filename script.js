08ced76fe25e408a4f14bd77a4f549aef59a6729
bans1mp <iamsarthakgandotra@gmail.com>
2022-01-12 19:49:21 +0530

    Styling and Alarm System
    Improved Styling and finished adding alarms

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-setInterval(setClock, 1000)
-const button = document.getElementById('btn')
-const list = document.getElementById('list');
+setInterval(setClock, 1000);
+setInterval(checkAlarm, 1000);
+const button = document.getElementById('btn');
+
+
+let currentAlarms = 0;
+const ring = new Audio('sounds/Alarm-clock-bell-ringing-sound-effect.mp3')
 
 
 const hourHand = document.querySelector('[data-hour-hand]')
@@ -17,27 +21,61 @@ function setClock() {
     setRotation(hourHand, hoursRatio)
 }
 
-
-
-
-function addAlarm(time) {
-    const li = document.createElement('li');
-    const hrs = Math.floor(time / 100);
-    const minutes = time % 100;
-    if (minutes != 0 && hrs != 0) {
-        li.innerHTML = `${hrs}:${minutes}`
-    } else if (minutes == 0 || hrs == 0) {
-        if (hrs == 0) {
-            li.innerHTML = `00:${minutes}`
-
-        }
-        else {
-            li.innerHTML = `${hrs}:00`
-        }
-
+function checkAlarm() {
+    const currentDate = new Date();
+    const hrs = currentDate.getHours();
+    const mins = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    let hrsString;
+    if (hrs % 10 != hrs) {
+        hrsString = `${hrs}`
+    } else {
+        hrsString = `0${hrs}`
+    }
+    let minsString;
+    if (mins % 10 != mins) {
+        minsString = `${mins}`
+    } else {
+        minsString = `0${mins}`
     }
 
-    list.appendChild(li);
+    const h1 = document.getElementById('h1');
+    const h2 = document.getElementById('h2');
+    const h3 = document.getElementById('h3');
+    const h4 = document.getElementById('h4');
+    const timings = [h1, h2, h3, h4];
+    for (let timing of timings) {
+
+        if (timing.textContent == `${hrsString}:${minsString}` && seconds == 0) {
+            ring.play();
+            break;
+        }
+        else continue;
+    }
+}
+
+function addAlarm() {
+    if (currentAlarms >= 4) return;
+    let input = prompt('Enter Time in HHMM 24-Hour Format!', '0000');
+    if (!input) return;
+    if (!checkValidity(parseFloat(input))) {
+        alert('Enter Valid Input');
+        return;
+    }
+    const hrs = input.substring(0, 2);
+    const mins = input.substring(2, 5);
+    const currentAlarm = document.getElementById(`alarm${currentAlarms + 1}`);
+    const alarmTime = document.getElementById(`h${currentAlarms + 1}`);
+    alarmTime.textContent = `${hrs}:${mins}`
+    currentAlarm.hidden = false;
+    currentAlarms++;
+
+}
+
+function checkValidity(num) {
+    if (num >= 2400 || num < 0) return false;
+    if (num % 100 > 59) return false;
+    return true;
 }
 
 function setRotation(element, rotationRatio) {
@@ -46,4 +84,4 @@ function setRotation(element, rotationRatio) {
 
 setClock()
 
-addAlarm(0000);
+
